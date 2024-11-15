34d869eea6490782046ad616fa6c833e19d3b221
John Assefa <johnassefaforbussines@gmail.com>
2024-11-15 14:03:56 -0800

    made some changes to the land

--- packages/playground/src/constants.ts
+++ packages/playground/src/constants.ts
@@ -3,7 +3,7 @@ export const DOCUMENT_LIFETIME_DURATION_S = 60 * 20;
 export const SLOT_WIDTH = 12;
 export const SLOT_DEPTH = 12;
 export const SLOT_BORDER_THICKNESS = 1;
-export const SPACE_BETWEEN_SLOTS = 3;
+export const SPACE_BETWEEN_SLOTS = 0.4;
 export const INTERACTIVE_SLOT_COUNT_X = 5;
 export const INTERACTIVE_SLOT_COUNT_Z = 4;
 
